dea85f8571084f5003391a19368c8bad8903c39f
PLTR878 <petelovepete2543@gmail.com>
2025-10-17 22:55:08 +0700

    Delete pages/alerts.js

--- pages/alerts.js
+++
@@ -1,9 +0,0 @@
-// ตัวอย่างเรียกใช้ใน Alerts.js
-useEffect(() => {
-  const loadMatches = async () => {
-    const res = await fetch("/api/scan-latest");
-    const data = await res.json();
-    setMatches(data.results || []);
-  };
-  loadMatches();
-}, []);
