bf8eaeb46458863d70d96dcea1589332f58dc113
PLTR878 <petelovepete2543@gmail.com>
2025-10-21 10:10:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: false,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  output: 'standalone',
   trailingSlash: true,
 };
 
