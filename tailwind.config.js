c00c3b1c009e4911ade46ca6bd6cf0ba1da66113
PLTR878 <petelovepete2543@gmail.com>
2025-10-21 10:12:56 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
     extend: {
-      colors: { brand: { DEFAULT: "#2563eb" } },
-      boxShadow: { soft: "0 8px 30px rgba(0,0,0,0.06)" }
+      colors: { brand: { DEFAULT: '#2563eb' } },
+      boxShadow: { soft: '0 8px 30px rgba(0,0,0,0.06)' },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [],
 };
