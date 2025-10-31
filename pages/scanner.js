6538487bdf432313b08a7a43e91e8c0f5abc9f8e
PLTR878 <petelovepete2543@gmail.com>
2025-10-31 14:52:13 +0700

    Create scanner.js

---
+++ pages/scanner.js
@@ -0,0 +1,9 @@
+import ScannerSwitcher from "../components/ScannerSwitcher";
+
+export default function ScannerPage() {
+  return (
+    <main className="min-h-screen bg-[#0b1220] text-white pb-16">
+      <ScannerSwitcher />
+    </main>
+  );
+}
