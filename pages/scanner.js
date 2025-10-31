397423d1dcdfc7a918a5917eb7990c34645fc396
PLTR878 <petelovepete2543@gmail.com>
2025-10-31 19:57:21 +0700

    Delete pages/scanner.js

--- pages/scanner.js
+++
@@ -1,9 +0,0 @@
-import ScannerSwitcher from "../components/ScannerSwitcher";
-
-export default function ScannerPage() {
-  return (
-    <main className="min-h-screen bg-[#0b1220] text-white pb-16">
-      <ScannerSwitcher />
-    </main>
-  );
-}
